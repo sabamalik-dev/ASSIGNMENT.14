db371a0e74b6e05d276f42feaae7bd751a30a167
sabamalik2575@gmail.com <sabamalik2575@gmail.com>
2024-12-27 19:08:27 +0500

    pushing the single file

---
+++ function in javascript/function.js
@@ -0,0 +1,187 @@
+//Write a function findMaxBetweenTwoNumbers(num1, num2) to return the maximum of two numbers.
+
+//function greeting(num1,operator,num2){
+  //  if(operator == "+"){
+    //return num1+num2;
+    //}
+    //else{
+       // return "incorrect"
+   // }
+
+//}
+//var result = greeting(30 ,"+" ,50);
+//var result2 =greeting(2,"$",50);
+
+//console.log (result);
+//console.log(result2);
+
+//Write a function findMaxBetweenThreeNumbers(num1, num2, num3) to return the maximum of three numbers.
+//var num1=prompt("Enter the first number");
+//var num2= prompt("Enter the second number");
+//var num3 = prompt("Enter the third number");
+
+//function greeting(num1,num2,num3){
+  //  if(num1>=num2){
+    //    return "num1"
+    //}
+    //else if (num2>=num1){
+      //  return "num2"
+    //}
+    //else {
+      //  return "num3"
+    //}
+
+//}
+//console.log("return")
+
+//var input=prompt("Enter the number");
+
+//function greeting(number){
+  //  if(num>0){
+    //   console.log("number is poistive")
+    //return "number is poistive"
+    //}
+    //else if (num<0){
+      //  console.log("num is negetive")
+    //return "num is negtive"
+    //}
+    //else if (num===0){
+      //  console.log ("num is zero")
+        //return "num is zero"
+    //}
+//}
+//var result=greeting(number);
+//console.log("result",result)
+
+//var num=5;
+//var num=11;
+
+//function greeting(num){
+  //if(5==0 & 11==0){
+    //return "num is divisible"
+  //}
+  //else{
+  //  return "num is not divisible"
+  //}
+//}
+//var result=greeting(num)
+//console.log("result",result)
+
+
+//number = prompt("Enter the number:");
+//function floo(number){
+  //if(number%2 == 0){
+  //return ("number is even ")
+  //}
+  //else {
+    //return("number is odd")
+  //}
+//}
+//var result=floo(number);
+//console.log(result)
+
+//var Year = 2021;
+//var year=parseInt(prompt("Enter the Year"));
+//function golden(Year){
+ // if(Year%2==0 && Year%2==0){
+   // return("Year is true")
+  //}
+  //else{
+    //return ("Year is false")
+  //}
+//}
+//var result=golden(Year);
+//console.log(result)
+
+
+var computer;
+var percentage=56;
+function greeting(percentage){
+  if(percentage>=90){
+    return ("A")
+  }
+  else if (percentage >=80){
+    return("B")
+  }
+  else if (percentage >=70){
+    return("C")
+  }
+  else if (percentage >=60){
+    return("D")
+  }
+  else if (percentage >=40){
+    return("E")
+  }
+  else if (percentage <=40){
+    return("F")
+  }
+
+}
+var result=greeting(percentage);
+console.log (result);
+
+
+var phy;
+var percentage=80;
+function greeting(percentage){
+if (percentage >=90){
+  return ("A")
+}
+else if (percentage >=80){
+  return("B")
+}
+else if (percentage >=70){
+  return("C")
+}
+else if (percentage >=60){
+  return("D")
+}
+else if (percentage >=40){
+  return("E")
+}
+}
+var result=greeting(percentage);
+console.log(result);
+
+
+var Chemistry;
+var percentage=60;
+function greeting(percentage){
+if (percentage >=90){
+  result ("A")
+}
+else if (percentage >=80){
+  return("B")
+}
+else if (percentage >=70){
+  return("C")
+}
+else if (percentage >=40){
+  return("D")
+} else if (percentage <=40){
+  return("E")
+}
+}
+var result=greeting(percentage);
+console.log(result);
+
+var Biology;
+var percentage=58;
+function greeting(percentage){
+if (percentage >=90){
+  result ("A")
+}
+else if (percentage >=80){
+  return("B")
+}
+else if (percentage >=70){
+  return("C")
+}
+else if (percentage >=40){
+  return("D")
+} else if (percentage <=40){
+  return("E")
+}
+}
+var result=greeting(percentage);
+console.log(result);
